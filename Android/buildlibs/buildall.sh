ba943828b08329cefea86e7a1d0c084c225c1784
ericoporto <ericoporto2008@gmail.com>
2019-09-29 10:38:23 -0300

    makes things fail faster

--- Android/buildlibs/buildall.sh
+++ Android/buildlibs/buildall.sh
@@ -6,27 +6,6 @@ pushd ../../libsrc
 ./download.sh
 popd
 
-# android-14 is the minimum we can go with current Android SDK
-PLATFORM=android-14
-
-# standalone toolchains cannot share same directory
-export NDK_STANDALONE=$NDK_HOME/platforms/$PLATFORM
-
-for arch in armeabi armeabi-v7a x86 mips 
-do
-	rm -rf ../nativelibs/$arch
-	mkdir -p ../nativelibs/$arch
-	pushd $arch
-	chmod +x *.sh
-	./lua.sh	
-	./ogg.sh	
-	./tremor.sh  # ("vorbis") requires ogg
-	./theora.sh  # requires ogg, "vorbis"
-	./allegro.sh # requires ogg, "vorbis", theora
-	./dumb.sh    # requires allegro
-	popd
-done
-
 # android-21 is the minimum we can go with current Android SDK for 64-bits
 PLATFORM=android-21
 
@@ -48,3 +27,26 @@ do
 	./dumb.sh    # requires allegro
 	popd
 done
+
+# android-14 is the minimum we can go with current Android SDK
+PLATFORM=android-14
+
+# standalone toolchains cannot share same directory
+export NDK_STANDALONE=$NDK_HOME/platforms/$PLATFORM
+
+for arch in armeabi armeabi-v7a x86 mips 
+do
+	rm -rf ../nativelibs/$arch
+	mkdir -p ../nativelibs/$arch
+	pushd $arch
+	chmod +x *.sh
+	./lua.sh	
+	./ogg.sh	
+	./tremor.sh  # ("vorbis") requires ogg
+	./theora.sh  # requires ogg, "vorbis"
+	./allegro.sh # requires ogg, "vorbis", theora
+	./dumb.sh    # requires allegro
+	popd
+done
+
+

c5276f30c356fea13dea6f85fef8209bdd2da20e
Nick Sonneveld <nick@sonneveld.name>
2019-10-04 19:18:39 +1000

    hq2x: standalone, doesn't depend on ags::Common

--- Engine/libsrc/hq2x/hq2x3x.cpp
+++ Engine/libsrc/hq2x/hq2x3x.cpp
@@ -17,7 +17,7 @@
 //Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 #include <stdlib.h>
-#include "core/types.h"
+#include <stdint.h>
 
 static int   LUT16to32[65536];
 static int   RGBtoYUV[65536];
